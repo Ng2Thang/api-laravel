6e10a47c94636c7a347530608cb3f6e254eac6fe
thang.nguyen <thang.nguyen@codecomplete.jp>
2020-01-21 15:56:44 +0700

    1231231cdadaw

--- resources/views/api.blade.php
+++
@@ -1,4 +0,0 @@
-<form action="{{ route('route_name') }}" method="post">
-    <input type="hidden" name="_method" value="PUT">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-</form>
